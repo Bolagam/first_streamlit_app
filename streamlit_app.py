e98ef82deb52d217a53253ebd7ba1650026e011a
Bolagam <133650186+Bolagam@users.noreply.github.com>
2023-05-16 14:22:04 +0530

    Update streamlit_app.py
    for practice

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@
 import streamlit 
 
 streamlit.title('My parents nwe helthy Dinner')
+
+streamlit.header ('Break fast menu')
+
+
+streamlit.text ('kale,spinach & rocket smoothie')
+
+streamlit.text ('Hard Boiled Free Range Egg')
