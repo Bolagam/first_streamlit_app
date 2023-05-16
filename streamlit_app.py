b9519e85469dd00ac02f624ba03ed7f5529c8bee
Bolagam <133650186+Bolagam@users.noreply.github.com>
2023-05-16 14:37:50 +0530

    Update streamlit_app.py
    Breakfast favorities

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,3 @@
 
 
-import streamlit 
-
-streamlit.title('mY Parents nwe helthy dinner')
-
-streamlit.header ('Break fast menu')
-
-
-streamlit.text ('omega 3 blueberry otmeal')
-
-streamlit.text ('kale,spinach & Rocket Smothie')
-
-
-streamlit.text ('Hard-Boiled Free Range egg')
-
+streamlit.header('Build your own fruit smoothi')
