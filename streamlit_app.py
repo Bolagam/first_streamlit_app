53d95b0b57cba9235a36e88cba3f4c3ccb2a4f93
Bolagam <133650186+Bolagam@users.noreply.github.com>
2023-05-18 11:44:51 +0530

    Update streamlit_app.py
    for practice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 
-import pandas
-streamlit.dataframe(my_fruit_list)
+{
+  "name": "Banana",
+  "id": 1,
+  "family": "Musaceae",
+  "order": "Zingiberales",
+  "genus": "Musa",
+  "nutritions": {
+    "calories": 96,
+    "fat": 0.2,
+    "sugar": 17.2,
+    "carbohydrates": 22,
+    "protein": 1
+  }
+}
