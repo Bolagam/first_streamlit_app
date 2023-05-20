537190f2211ffaf912539dde756fe886781f7b68
Bolagam <133650186+Bolagam@users.noreply.github.com>
2023-05-20 18:09:11 +0530

    Update streamlit_app.py
    for practice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
-import streamlit
-
-streamlit.title('My Parents Nwe Healthy Diner')
-
-import streamlit 
-
-streamlit.title('My Parents New Healthy Diner')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale ,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Bolied Free-Range Egg')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
