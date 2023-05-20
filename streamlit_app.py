bd4cb33f95dd67b7af02e733f8f0918e77299b5b
Bolagam <133650186+Bolagam@users.noreply.github.com>
2023-05-20 17:41:29 +0530

    Update streamlit_app.py
    for practice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
 
-import  streamlit
-
-streamlit.title('My Parents New Healthy Diner')
- 🥣 🥗 🐔 🥑🍞
-
-import streamlit
-streamlit.title ('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale,spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
 
 import streamlit
 streamlit.title ('My Mom s New Healthy Diner')
@@ -18,6 +7,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-                 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-                 
+               
+ 
+ 
