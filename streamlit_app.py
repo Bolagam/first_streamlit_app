5cc47989d1fa58249bdaf8804057a9055c288647
Bolagam <133650186+Bolagam@users.noreply.github.com>
2023-05-23 23:12:55 +0530

    Update streamlit_app.py
    for practice

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇'
 
 
